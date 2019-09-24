7aa8320037209c26e8114942e3631a856ecb495d
Lcole.engr <lcole.engr@gmail.com>
2019-09-24 12:53:18 -0400

    Coding 80% complete, fixed multchoice bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,13 @@ var scantron = ["A", "B", "C", "D"];
 var intervalId;
 var category;
 var qTimer;                 //Variable for the 30 second question timer
-var quesAmount;           // Variable that keeps track of how many questions are asked in the round. For this game the player will get 5 total questions
+var quesAmount= 0;           // Variable that keeps track of how many questions are asked in the round. For this game the player will get 5 total questions
 var correctCount;         // Variable that keeps track of how many questions the player gets correct in a round
 var incorrectCount;         // Variable that keeps track of how many questions the player gets incorrect in a round
 var contestantScore;        //Variable used to present contestant's percentage score (correctCount/quesAmount * 100)
-
+var qAnswered;
+var qUnanswered;
+var selResponse;
 
 
 
@@ -28,11 +30,11 @@ var contestantScore;        //Variable used to present contestant's percentage s
 
 var basicQuestions = {
 
-  ask: ["Name the number that is three more than one-fifth of one-tenth of one-half of 5000:", "What is the only U.S. State that only borders one other?", "What's the missing number?", "What's the oldest continuously inhabited city in the world?", "Two people are standing back to back. They each walk away from each other for 3 feet. Then, they both turn left and walk for another four feet, and then stop. Now how many feet apart are they standing?"],
+  ask: ["Name the number that is three more than one-fifth of one-tenth of one-half of 5000:", "What is the only U.S. State that only borders one other?", "What's the missing number?", "What's the oldest continuously inhabited city in the world?", "What is the highest score possible with three darts on a standard dart board? "],
 
-  choices: [[503, 103, 53, 108], ["Rhode Island", "Maine", "Washington", "Florida"],[20, 21, 25, 27],["Istanbul, Turkey", "Athens, Greece", "Jerusalem", "Damascus, Syria" ],[10, 7, 25, 5]],
+  choices: [[503, 103, 53, 108], ["Rhode Island", "Maine", "Washington", "Florida"],[20, 21, 25, 27],["Istanbul, Turkey", "Athens, Greece", "Jerusalem", "Damascus, Syria" ],[100, 300, 150, 180]],
 
-  answers:[53, "Maine", 21, "Damascus, Syria", 10]
+  answers:[53, "Maine", 21, "Damascus, Syria", 180]
  
 }
 
@@ -43,7 +45,6 @@ var sportsQuestions = {
   choices:[["Indiana Pacers", "New York Knicks", "Milwaukee Bucks", "Houston Rockets"], [11, 10, 12, 13] , ["Germany", "Brazil", "England", "Portugal"] , ["30 minutes", "5 minutes", "1 hour", "10 minutes"], ["Polo", "Soccer", "Rugby", "Cricket"]],
 
   answers:["Indiana Pacers", 12, "Brazil", "5 minutes", "Polo"]
-
 
 
 }
@@ -75,11 +76,23 @@ var responses = {
 
 // ******************************************************END OF OBJECTS FOR GAMEPLAY**************************************************
 
+
+
+
+// Hover Function%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
  
-//  *************************************TIMER FUNCTION***************************************************
+//  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^TIMER FUNCTION^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  
  function run() {
     clearInterval(intervalId);
+    qAnswered = true;
     intervalId = setInterval(decrement, 1000);
   }
 
@@ -102,51 +115,185 @@ function decrement() {
     $("#countdown").text("Time Remaining: "+ number);
 
     //  Once number hits zero...
-    if (number === 0) {
+    if (number < 1) {
 
-      //  ...run the stop function.
       stop();
+      qAnswered = false;
+      checkAns();
     }
 }
 
-run();
-decrement();
+
 
 // ****************************************END OF TIME FUNCTION***********************************************************************
 
 // ****************************OTHER FUNCTIONS**************************************************************************
 
-for ( quesAmount=0; quesAmount < 5; quesAmount++){
-  var categoryChoice= basicQuestions;
+
+function setQuestion(){
+
+  
+  var topic = (Math.floor(Math.random() * 3)); // Allows for randomly choosing category of questions
+  var categoryChoice;
+  if (topic===0){
+    categoryChoice = basicQuestions;
+
+  } else if(topic === 1){
+
+    categoryChoice = sportsQuestions;
+  } else {
+
+    categoryChoice= impossibleQuestions;
+  }
+console.log(topic);
+console.log(categoryChoice);
+
+  
   var askDiv = $("<div>");
   askDiv.addClass("quesPlace");
   $(".questionsdiv").append(askDiv);
-  $(".quesPlace").text(categoryChoice.ask[0]);
-
-  console.log(categoryChoice.ask[0]);
-  console.log(categoryChoice.ask[1]);
+  $(".quesPlace").html(categoryChoice.ask[quesAmount]);
   
 
-  for (var a=0; a < 4; a++){
+  for (var a=0; a < scantron.length; a++){
 
-
+// debugger;
     var multChoiceDiv = $("<div>");
     multChoiceDiv.addClass("ansOptions");
-    $(".ansOptions").text(scantron[a] + "  " + categoryChoice.choices[a][0]);
-    $(".ansOptions").text(scantron[a] + "  " + categoryChoice.choices[a][1]);
-    $(".ansOptions").text(scantron[a] + "  " + categoryChoice.choices[a][2]);
-    $(".ansOptions").text(scantron[a] + "  " + categoryChoice.choices[a][3]);
+
+    multChoiceDiv.html(categoryChoice.choices[quesAmount][a]);
+
+    // multChoiceDiv.attr("data-id", categoryChoice.choices[quesAmount][a]);
     $(".answerchoicesdiv").append(multChoiceDiv);
-    console.log(scantron[a] + categoryChoice.choices[0][a]);
+    var datavalue= $(this).data("id");
+    // $(".ansOptions").html("<h3>" + $(this).data("id")+ "</h3>");
+    // $(".ansOptions").text(scantron[a] + "   " + categoryChoice.choices[quesAmount][a]);
+    // $(".ansOptions").text($(this).data("id"));
+   
+    console.log(scantron[a] + categoryChoice.choices[quesAmount][a]);
   }
 
 
+
+  run();          //Sets up Clock
+  decrement();      //Counts down the Clock
+
+  $(".answerchoicesdiv").on("click", function(){
+
+     selResponse = ($(this).data("data-value"));
+
+    stop();
+    checkAns();
+
+
+  });
 
 }
 
 
 
-
-
-
 //********************************************GAMEPLAY ***************************************************************************8 */
+
+$(document).ready(function(){
+
+setQuestion();    //Sets up Question and Multiple Choice to be selected
+// run();          //Sets up Clock
+// decrement();      //Counts down the Clock
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function checkAns(){
+
+    var ran = (Math.floor(Math.random() * 8));   // Allows for randomly selecting message
+
+    $(".questionsdiv").empty();
+    $(".answerchoicesdiv").empty();
+
+    if ((selResponse === categoryChoice.answers[quesAmount])&&(qAnswered === true)){
+
+      correctCount ++;
+      // place code here for message to be displayed
+      $(".rightorwrong").text(responses.good[ran]);
+
+    } else if((selResponse !== categoryChoice.answers[quesAmount])&&(qAnswered === true)){
+
+      incorrectCount ++;
+      // place code to let user know what the correct answer was
+
+      $(".rightorwrong").text("The correct answer was: " + categoryChoice.answers[quesAmount]);
+
+      //place code here for message to be displayed
+
+      $(".rightorwrong").text(responses.bad[ran]);
+    } else {
+
+      qUnanswered ++;
+
+      // place code for you ran out of time and were not able to make a selection
+
+      //place code to let user know what the correct answer was
+
+      qanswered= true;          //reset the qanswered for other questions
+    }
+
+    if (quesAmount === 4){
+
+      setTimeout(finalscore, 5000);
+
+    } else{
+
+      quesAmount ++;
+      setTimeout(setQuestion, 5000);
+    }
+
+  }
+
+
+
+
+
+
+
+  function finalscore(){
+    $('#countdown').empty();
+    $('.rightorwrong').empty();
+   
+    // $('#gif').empty();
+
+  
+    $('#finalMessage').html(messages.finished);
+    $('#correctAnswers').html("Correct Answers: " + correctAnswer);
+    $('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+    $('#unanswered').html("Unanswered: " + unanswered);
+    $('#restartBtn').addClass('reset');
+    $('#restartBtn').show();
+    $('#restartBtn').html('Start Over?');
+  }
